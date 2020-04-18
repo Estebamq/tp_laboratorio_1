2f233ca5209bba7ae4af17e5a91d16e7ed23651d
Estebanmq <quiroz_estebanmarcelo@live.com>
2020-04-17 22:10:35 -0300

    Calculadora Ultima

--- tp_1/calculo.c
+++ tp_1/calculo.c
@@ -1,1 +1,98 @@
 
+#include "calculo.h"
+
+
+
+
+/** \brief Funcion Suma
+ *
+ * \param a int
+ * \param b int
+ * \return int
+ *
+ */
+int sumar(int a,int b){
+int resultado;
+
+resultado = a + b;
+
+return resultado;
+
+}
+
+
+
+/** \brief Funcion Resta
+ *
+ * \param a int
+ * \param b int
+ * \return int
+ *
+ */
+int restar(int a, int b){
+int resultado;
+
+resultado = a - b;
+
+return resultado;
+}
+
+
+
+/** \brief Funcion Dividir
+ *
+ * \param a int
+ * \param b int
+ * \return float
+ *
+ */
+float dividir(int a, int b){
+float resultado;
+
+resultado = (float)a / b;
+
+return resultado;
+}
+
+
+
+/** \brief Funcion Multiplicar
+ *
+ * \param a int
+ * \param b int
+ * \return int
+ *
+ */
+int multiplicar(int a, int b){
+int resultado;
+
+resultado = a*b;
+
+return resultado;
+}
+
+
+/** \brief Funcion Factorial
+ *
+ * \param a int
+ * \return int
+ *
+ */
+int factorial(int a)
+{
+
+   int resultado;
+
+    if (a == 0)
+        {
+             resultado = 1;
+        }
+
+    else
+    {
+        resultado = a* factorial(a-1);
+    }
+    return resultado;
+}
+
+
--- tp_1/calculo.h
+++ tp_1/calculo.h
@@ -1,1 +1,8 @@
+#include <stdio.h>
+
+int sumar(int a,int b);
+int restar(int a, int b);
+float dividir(int a, int b);
+int multiplicar(int a, int b);
+int factorial(int a);
 
